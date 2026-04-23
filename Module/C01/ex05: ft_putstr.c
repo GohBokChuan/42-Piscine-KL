faeb27ff2f84a5f9d54d575691cd681ffcf45b63
GohBokChuan <84007752+GohBokChuan@users.noreply.github.com>
2026-04-23 19:49:22 +0800

    Create ex05: ft_putstr.c

---
+++ Module/C01/ex05: ft_putstr.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+
+//-Version 1-
+void ft_putstr(char *str)
+{
+    int i;
+    i = 0;
+    while(str[i] != '\0')
+   {
+       write(1, &str[i], 1);
+       //printf("%c", str[i]); --->Other method
+       i++;
+    }
+}
+
+int main()
+{
+    char string[] = "Hello, my name is Bok Chuan";
+    //char *string = "Hello, my name is Bok Chuan"; ---->Other method
+    
+    ft_putstr(string);
+    return 0;
+}
+
+//Miscellanous method
+
+void ft_putstr(char *str)
+{
+    printf("%s",str);
+}
+
+int main()
+{
+    char string[] = "Hello, my name is Bok Chuan";
+    
+    ft_putstr(string);    
+    return 0;
+}
