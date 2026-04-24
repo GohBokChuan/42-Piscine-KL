f81d083bb242b114352bfc3c839e8b2f026ee23a
GohBokChuan <84007752+GohBokChuan@users.noreply.github.com>
2026-04-24 12:31:24 +0800

    Update and rename ex02 : ft_str_is_alpha to ex02 : ft_str_is_alpha.c

---
+++ Module/C02/ex02 : ft_str_is_alpha.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#include <stdio.h>
+
+
+int ft_str_is_alpha(char *str)
+{
+    int i;
+    i = 0;
+    while(str[i] != '\0')
+    {
+        if(!((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')))
+        {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+
+int main() 
+{
+    char str1[] = "Check the string char here";
+    char str2[] = "Check the string !";
+    char str3[] = "Checking";
+    
+    printf("This is string 1 result: %d\n",ft_str_is_alpha(str1));
+    printf("This is string 2 result: %d\n",ft_str_is_alpha(str2));
+    printf("This is string 3 result: %d\n",ft_str_is_alpha(str3));
+
+    return 0;
+}
