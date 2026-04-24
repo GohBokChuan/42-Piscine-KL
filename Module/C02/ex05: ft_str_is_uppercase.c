6ff87e017f84de101d0b1963135140d3be0d3379
GohBokChuan <84007752+GohBokChuan@users.noreply.github.com>
2026-04-24 13:21:25 +0800

    Update ex05: ft_str_is_uppercase.c

--- Module/C02/ex05: ft_str_is_uppercase.c
+++ Module/C02/ex05: ft_str_is_uppercase.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int ft_str_is_uppercase(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        if (!(str[i] >= 'A' && str[i] <= 'Z'))
+        {
+            return (0); 
+        }
+        i++;
+    }
+    return (1);
+}
+
+int main(void)
+{
+    char *str1 = "abcde";
+    char *str2 = "A B C D E"; 
+    char *str3 = "ABCDe0!?34578"; 
+    char *str4 = "";
+    char *str5 = "ABC";
+
+    printf("Test 1 Result: %d\n", ft_str_is_uppercase(str1));
+    printf("Test 2 Result: %d\n", ft_str_is_uppercase(str2));
+    printf("Test 3 Result: %d\n", ft_str_is_uppercase(str3));
+    printf("Test 4 Result: %d\n", ft_str_is_uppercase(str4));
+    printf("Test 4 Result: %d\n", ft_str_is_uppercase(str5));
+
+    return (0);
+}
