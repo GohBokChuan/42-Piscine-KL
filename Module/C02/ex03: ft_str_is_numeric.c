fe93a83b3e2ef32b98536d1d6f5dbc73d8cdbb13
GohBokChuan <84007752+GohBokChuan@users.noreply.github.com>
2026-04-24 12:59:37 +0800

    Create ex03: ft_str_is_numeric.c

---
+++ Module/C02/ex03: ft_str_is_numeric.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int ft_str_is_numeric(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        if (!(str[i] >= '0' && str[i] <= '9'))
+        {
+            return (0); 
+        }
+        i++;
+    }
+
+    return (1);
+}
+
+int main(void)
+{
+    char *str1 = "012345678";
+    char *str2 = "0 1 2 3 4 5 7 8"; 
+    char *str3 = "0!?34578"; 
+    char *str4 = " "; 
+
+    printf("Test 1 Result: %d\n", ft_str_is_numeric(str1));
+    printf("Test 2 Result: %d\n", ft_str_is_numeric(str2));
+    printf("Test 3 Result: %d\n", ft_str_is_numeric(str3));
+    printf("Test 4 Result: %d\n", ft_str_is_numeric(str4));
+
+    return (0);
+}
