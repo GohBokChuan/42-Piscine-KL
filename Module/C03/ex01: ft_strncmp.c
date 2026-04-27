cc50e4fce0d7d869fbbd2936b56c4fe0d6be4576
GohBokChuan <84007752+GohBokChuan@users.noreply.github.com>
2026-04-27 20:17:51 +0800

    Update and rename ex01: strncmp.c to ex01: ft_strncmp.c

---
+++ Module/C03/ex01: ft_strncmp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+    int i;
+    i = 0;
+    
+    while(s1[i] == s2[i] && s1[i] != '\0' && i < n - 1)
+    {
+        i++;
+    }
+    return (s1[i] - s2[i]);
+}
+
+int main() 
+{
+    char s1[] = "ABCDE";
+    char s2[] = "ABCDEF";
+    int n = 6;
+    int result;
+    result = ft_strncmp(s1, s2, 6);
+    
+    printf("The result of the string compare between s1 and s2 is %d\n", result);
+    return 0;
+}
