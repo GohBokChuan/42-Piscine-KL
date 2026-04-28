08695579111ef21beead9d1f488f5f98caf39e70
GohBokChuan <84007752+GohBokChuan@users.noreply.github.com>
2026-04-28 16:09:04 +0800

    Create ex01: ft_ultimate_ft.c

---
+++ Module/C01/ex01: ft_ultimate_ft.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+/*
+int     main()
+{
+        int a = 12;
+        int *b = &a;
+        int **c = &b;
+        int ***d = &c;
+        int ****e = &d;
+        int *****f = &e;
+        int ******g = &f;
+        int *******h = &g;
+        int ********i = &h;
+        int *********nbr = &i;
+	
+	ft_ultimate_ft(&i);
+	printf("%d\n", *********nbr);
+	return 0;
+}*/
