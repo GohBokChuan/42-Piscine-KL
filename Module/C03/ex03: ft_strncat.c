5c3bc5bbfe2a181cd96e19329b7c346afe8fd4a2
GohBokChuan <84007752+GohBokChuan@users.noreply.github.com>
2026-04-29 23:28:39 +0800

    Update ex03: ft_strncat.c

--- Module/C03/ex03: ft_strncat.c
+++ Module/C03/ex03: ft_strncat.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
 
+char *ft_strcat(char *dest, char *src, unsigned int nb)
+{
+    int i;
+    i = 0;
+    
+    int j;
+    j = 0;
+    
+    while(dest[i] != '\0')
+    {
+        i++;
+    }
+    
+    while(src[j] != '\0' && i < nb)
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
+    
+    return dest;
+}
+// --------------End of Code---------------
+
+int main() 
+{
+    char dest[50] = "Hello";
+    char src[] = "There";
+
+    ft_strcat(dest, src, 15);
+    
+    printf("This is my string: %s\n", dest);
+    return 0;
+}
