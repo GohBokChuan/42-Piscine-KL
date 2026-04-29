873f1dd82c63080c3fabe8a8dfecc1ee2b69a004
GohBokChuan <84007752+GohBokChuan@users.noreply.github.com>
2026-04-29 17:16:13 +0800

    Update ex02: ft_strcat.c

--- Module/C03/ex02: ft_strcat.c
+++ Module/C03/ex02: ft_strcat.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
 
+char *ft_strcat(char *dest, char *src)
+{
+    int i;
+    i = 0;
+    
+    int j;
+    j = 0;
+    
+    while(dest[i] != '\0')
+    {
+        i++;
+    }
+    
+    while(src[j] != '\0')
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
+    
+    return dest;
+}
+// --------------End of Code---------------
+
+int main() 
+{
+    char dest[50] = "Hello";
+    char src[] = "There";
+
+    ft_strcat(dest, src);
+    
+    printf("This is my string: %s\n", dest);
+    return 0;
+}
