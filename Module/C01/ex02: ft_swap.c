20bf05ca8aef1355aa8db6f573ed031464d69535
GohBokChuan <84007752+GohBokChuan@users.noreply.github.com>
2026-04-30 16:23:12 +0800

    Create ex02: ft_swap.c

---
+++ Module/C01/ex02: ft_swap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/*
+int	main (void)
+{
+	int a = 10;
+	int b = 20;
+	ft_swap(&a, &b);
+	printf("a = %d\n", a);
+	printf("b = %d\n", b);
+}*/
