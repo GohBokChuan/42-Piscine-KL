362808d7edc140d2b3ef01cd98894cc60de3094a
GohBokChuan <84007752+GohBokChuan@users.noreply.github.com>
2026-04-30 17:28:47 +0800

    Update ex00: ft_ft.c

--- Module/C01/ex00: ft_ft.c
+++ Module/C01/ex00: ft_ft.c
@@ -6,14 +6,21 @@ void	ft_ft(int *nbr)
 }
 //Function above means "Go to the address in nbr
 //and change the value there to 42"
-/*int     main(void)
+int     main(void)
 {
         int n = 10;
-        int *nbr = &n;
+        int *nbr = &n; //optional --->this apply
 
-        ft_ft(&n);
+        ft_ft(nbr); //----->this must apply
         printf("%d\n", n);
-}*/
+}
 //int *nbr "I am holding the address of an integer"
 //nbr "The address itself"
 //*nbr "The value at the address." ~Dereference
+int     main(void)
+{
+        int n = 10;
+
+        ft_ft(&n);
+        printf("%d\n", n);
+}
