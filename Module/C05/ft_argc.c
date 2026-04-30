3b01ef0ea7f499a9127d7c6706bcedded7b28183
GohBokChuan <84007752+GohBokChuan@users.noreply.github.com>
2026-05-01 01:49:10 +0800

    Update ft_argc.c

--- Module/C05/ft_argc.c
+++ Module/C05/ft_argc.c
@@ -1,1 +1,27 @@
+#include <unistd.h>
 
+int main (int argc, char **argv)
+{
+    if (argc == 3)
+    {
+        int i;
+        i = 0;
+        
+        int j;
+        j = 0;
+        
+        while(argv[1][i])
+        {
+            write(1, &argv[1][i], 1);
+            i++;
+        }
+        write(1, "\n", 1);
+        
+        while(argv[2][j])
+        {
+            write(1, &argv[2][j], 1);
+            j++;
+        }
+    }
+    write(1, "\n", 1);
+}
