1ae54b943eeaea007a94045b9be4e84f8df10d5d
GohBokChuan <84007752+GohBokChuan@users.noreply.github.com>
2026-04-30 23:31:03 +0800

    Create ex07: ft_strupcase.c

---
+++ Module/C02/ex07: ft_strupcase.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
+
+int	main(void)
+{
+	char	str1[] = "hello there";
+	char	str2[] = "hElLo ThErE";
+	char	str3[] = "hello 123!";
+
+	printf("str1 result: %s\n", ft_strupcase(str1));
+	printf("str2 result: %s\n", ft_strupcase(str2));
+	printf("str3 result: %s\n", ft_strupcase(str3));
+	return (0);
+}
