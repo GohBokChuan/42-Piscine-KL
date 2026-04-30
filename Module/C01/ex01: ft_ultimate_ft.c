3f6d228531e45acbd3273d8e5ecb8f94e7ec6eef
GohBokChuan <84007752+GohBokChuan@users.noreply.github.com>
2026-04-30 16:20:21 +0800

    Update ex01: ft_ultimate_ft.c

--- Module/C01/ex01: ft_ultimate_ft.c
+++ Module/C01/ex01: ft_ultimate_ft.c
@@ -20,7 +20,27 @@ int     main()
         int ********i = &h;
         int *********nbr = &i;
 	
-	ft_ultimate_ft(&i);
+	ft_ultimate_ft(nbr);
 	printf("%d\n", *********nbr);
 	return 0;
+}
+
+//Miscellanous Method
+
+int main()
+{
+    int a = 12;
+    int *b = &a;
+    int **c = &b;
+    int ***d = &c;
+    int ****e = &d;
+    int *****f = &e;
+    int ******g = &f;
+    int *******h = &g;
+    int ********i = &h;
+    //int *********nbr = &i;    // ← no need for this!
+
+    ft_ultimate_ft(&i);         // &i becomes 9 stars on the spot
+    printf("%d\n", a);
+    return 0;
 }
